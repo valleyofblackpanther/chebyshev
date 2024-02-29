04a1f51f8b23d95631febb0327b952b62c6df8c8
unknown <kaushikam12@gmail.com>
2024-02-29 12:20:22 +0000

    new merges

--- hyper.py
+++
@@ -1,8 +0,0 @@
-def create_breakpoints(n):
-    # Create n+1 breakpoints between 0 and 1
-    breakpoints = [i/(2**n) for i in range(2**n + 1)]
-    return breakpoints
-
-n = 3  # For example, if n=3, it creates 2^3 intervals
-breakpoints = create_breakpoints(n)
-print(breakpoints)
